c6bb5d32ab38223d3be5988c54dbeda843f8468c
Sebastian Arlt <s.arlt@bin-control.com>
2019-06-19 14:55:58 +0200

    Projektdateien hinzufügen.

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnl_player1 = new System.Windows.Forms.Panel();
+            this.pnl_player2 = new System.Windows.Forms.Panel();
+            this.pnl_ball = new System.Windows.Forms.Panel();
+            this.lbl_counter1 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.tmr_ball = new System.Windows.Forms.Timer(this.components);
+            this.lbl_counter2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnl_player1
+            // 
+            this.pnl_player1.BackColor = System.Drawing.Color.White;
+            this.pnl_player1.Location = new System.Drawing.Point(39, 198);
+            this.pnl_player1.Name = "pnl_player1";
+            this.pnl_player1.Size = new System.Drawing.Size(20, 84);
+            this.pnl_player1.TabIndex = 0;
+            // 
+            // pnl_player2
+            // 
+            this.pnl_player2.BackColor = System.Drawing.Color.White;
+            this.pnl_player2.Location = new System.Drawing.Point(1053, 219);
+            this.pnl_player2.Name = "pnl_player2";
+            this.pnl_player2.Size = new System.Drawing.Size(20, 84);
+            this.pnl_player2.TabIndex = 1;
+            // 
+            // pnl_ball
+            // 
+            this.pnl_ball.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnl_ball.Location = new System.Drawing.Point(430, 315);
+            this.pnl_ball.Name = "pnl_ball";
+            this.pnl_ball.Size = new System.Drawing.Size(15, 19);
+            this.pnl_ball.TabIndex = 2;
+            // 
+            // lbl_counter1
+            // 
+            this.lbl_counter1.AutoSize = true;
+            this.lbl_counter1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_counter1.ForeColor = System.Drawing.Color.Lime;
+            this.lbl_counter1.Location = new System.Drawing.Point(206, 28);
+            this.lbl_counter1.Name = "lbl_counter1";
+            this.lbl_counter1.Size = new System.Drawing.Size(24, 17);
+            this.lbl_counter1.TabIndex = 0;
+            this.lbl_counter1.Text = "00";
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.Transparent;
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.ForeColor = System.Drawing.Color.White;
+            this.btn_start.Location = new System.Drawing.Point(524, 28);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(99, 32);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // tmr_ball
+            // 
+            this.tmr_ball.Interval = 3;
+            this.tmr_ball.Tick += new System.EventHandler(this.tmr_ball_Tick);
+            // 
+            // lbl_counter2
+            // 
+            this.lbl_counter2.AutoSize = true;
+            this.lbl_counter2.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_counter2.ForeColor = System.Drawing.Color.Red;
+            this.lbl_counter2.Location = new System.Drawing.Point(889, 28);
+            this.lbl_counter2.Name = "lbl_counter2";
+            this.lbl_counter2.Size = new System.Drawing.Size(24, 17);
+            this.lbl_counter2.TabIndex = 4;
+            this.lbl_counter2.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1116, 597);
+            this.Controls.Add(this.lbl_counter2);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.lbl_counter1);
+            this.Controls.Add(this.pnl_ball);
+            this.Controls.Add(this.pnl_player2);
+            this.Controls.Add(this.pnl_player1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_player1;
+        private System.Windows.Forms.Panel pnl_player2;
+        private System.Windows.Forms.Panel pnl_ball;
+        private System.Windows.Forms.Label lbl_counter1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Timer tmr_ball;
+        private System.Windows.Forms.Label lbl_counter2;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+
+    {
+        bool goup;
+        bool godown;
+        int speed = 5;
+        int ballX;
+        int ballY = 5;
+        int score = 0;
+        int lblcounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            
+            
+                pnl_ball.Location = new Point(420, 195);
+                tmr_ball.Enabled = true;
+            
+
+        }
+
+        private void tmr_ball_Tick(object sender, EventArgs e)
+        {
+            lbl_counter1.Text = "" + score;
+            lbl_counter2.Text = "" + lblcounter;
+
+            pnl_ball.Top -= ballY;
+            pnl_ball.Left -= ballX;
+
+            pnl_player2.Top += speed;
+
+            if (score < 5)
+            {
+                if (pnl_player2.Top < 0 || pnl_player2.Top > 455)
+                {
+                    speed = -speed;
+                }
+            }
+            else
+            {
+                pnl_player2.Top = pnl_ball.Top + 30;
+            }
+            
+            if (pnl_ball.Left < 0)
+            {
+                pnl_ball.Left = 0;
+                ballX = -ballX;
+                ballX -= 2;
+                lblcounter++;
+            }
+            
+            if (pnl_ball.Left + pnl_ball.Width > ClientSize.Width)
+            {
+                pnl_ball.Left = 0;
+                ballX = -ballX;
+                ballX += 2;
+                score++;
+            }
+
+            if (pnl_ball.Top < 0 || pnl_ball.Top + pnl_ball.Height > ClientSize.Height)
+            {
+                ballY = -ballY;
+            }
+
+            if (pnl_ball.Bounds.IntersectsWith(pnl_player1.Bounds) || pnl_ball.Bounds.IntersectsWith(pnl_player2.Bounds))
+            {
+                ballX = -ballX;
+            }
+
+            if (goup == true && pnl_player1.Top > 0)
+            {
+                pnl_player1.Top -= 8;
+            }
+
+            if (godown == true && pnl_player1.Top < 455)
+            {
+                pnl_player1.Top += 8;
+            }
+
+            if (score > 10)
+            {
+                tmr_ball.Stop();
+                MessageBox.Show("Herzlichen Glükwunsch! Du hast gewonnen");
+            }
+
+            if (lblcounter > 10)
+            {
+                tmr_ball.Stop();
+                MessageBox.Show("Schade! Du hast verloren");
+            }
+
+        }
+private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+                
+        }
+    }
+}
